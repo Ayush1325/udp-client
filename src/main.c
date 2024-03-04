2c68ea11dd2aca491542e83aefa4d83c2eca1c11
Ayush Singh <ayushdevel1325@gmail.com>
2024-03-05 02:06:22 +0530

    Update for TCP tests
    Signed-off-by: Ayush Singh <ayushdevel1325@gmail.com>

--- src/main.c
+++ src/main.c
@@ -6,15 +6,17 @@
 
 #include <zephyr/logging/log.h>
 #include <zephyr/net/socket.h>
+#include <zephyr/timing/timing.h>
 
 #define PORT 4242
+#define REQS 50
 
 const int num_packets = 100;
 
 LOG_MODULE_REGISTER(udp_client, LOG_LEVEL_DBG);
 
 int server_init() {
-  int sock = zsock_socket(AF_INET6, SOCK_DGRAM, IPPROTO_UDP);
+  int sock = zsock_socket(AF_INET6, SOCK_STREAM, IPPROTO_TCP);
 
   if (sock < 0) {
     LOG_ERR("socket failed: %d", -errno);
@@ -25,27 +27,61 @@ int server_init() {
 }
 
 int main(void) {
-  uint8_t buf[100] = {1};
-  int i, sock;
+  timing_t start_time, end_time;
+  uint64_t total_cycles, total_ns;
+  int sock, ret, buf = 0, i;
   struct sockaddr_in6 server_addr = {
       .sin6_family = AF_INET6,
       .sin6_port = htons(PORT),
   };
   inet_pton(AF_INET6, CONFIG_NET_CONFIG_PEER_IPV6_ADDR, &server_addr.sin6_addr);
 
+  timing_init();
+
   sock = server_init();
   if (sock < 0) {
     return sock;
   }
 
-  LOG_INF("UDP server started");
+  LOG_DBG("Socket Created");
 
-  for (i = 0; i < num_packets; i++) {
-    zsock_sendto(sock, buf, sizeof(buf), 0, (struct sockaddr *)&server_addr,
-                 sizeof(server_addr));
-		LOG_DBG("Sent pkg: %d", i);
-    k_sleep(K_MSEC(50));
+  ret =
+      zsock_connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr));
+  if (ret < 0) {
+    LOG_ERR("connect failed: %d", -errno);
+    return -1;
   }
+
+  LOG_INF("TCP client started");
+
+  k_sleep(K_MSEC(1000));
+
+  timing_start();
+  start_time = timing_counter_get();
+
+  for (i = 0; i < REQS; ++i) {
+    ret = zsock_send(sock, &i, sizeof(i), 0);
+    if (ret < 0) {
+      LOG_ERR("send failed: %d", -errno);
+      return -1;
+    }
+
+    ret = zsock_recv(sock, &buf, sizeof(buf), 0);
+    if (ret < sizeof(i) || buf != i) {
+      LOG_ERR("recv failed: ret: %d, err: %d, buf: %d", ret, -errno, buf);
+      return -1;
+    }
+  }
+
+  end_time = timing_counter_get();
+
+  total_cycles = timing_cycles_get(&start_time, &end_time);
+  total_ns = timing_cycles_to_ns(total_cycles);
+
+  LOG_INF("Total time: %llu ns\n", total_ns);
+  LOG_INF("Time per request: %llu ns\n", total_ns / REQS);
+
+  timing_stop();
 
   return 0;
 }
